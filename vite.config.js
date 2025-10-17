a3fd8782b7ca27cd06f50786a1919e2b23c9c90e
f.celletti <fabrus98@gmail.com>
2025-10-17 15:38:55 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
     server: {
       host: '87.106.39.97',
       port: 5173,
-  },
-})
+      proxy: {
+        '/api': {
+          target: backendUrl,  // backend Django
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
+)
