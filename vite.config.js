4350f5c007533b8460ddd0d7e9bed132ffc42af2
f.celletti <fabrus98@gmail.com>
2025-10-17 15:41:03 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       port: 5173,
       proxy: {
         '/api': {
-          target: import.meta.env.VITE_BACKEND_URL,  // backend Django
+          target: 'http://127.0.0.1:8000',  // backend Django
           changeOrigin: true,
           secure: false,
         },
